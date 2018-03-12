1e18da3b86272fecfd7dea399c2c0547a4789cf5
xuziang111 <584788096@qq.com>
2018-03-12 18:52:22 +0800

    修复后台链接

--- src/js/admin/upload-img.js
+++ src/js/admin/upload-img.js
@@ -23,7 +23,7 @@
             // Ajax请求downToken的Url，私有空间时使用,JS-SDK 将向该地址POST文件的key和domain,服务端返回的JSON必须包含`url`字段，`url`值为该文件的下载地址
             // unique_names: true,              // 默认 false，key 为文件名。若开启该选项，JS-SDK 会为每个文件自动生成key（文件名）
             // save_key: true,                  // 默认 false。若在服务端生成 uptoken 的上传策略中指定了 `save_key`，则开启，SDK在前端将不对key进行任何处理
-            domain: 'p5gyxlxbh.bkt.clouddn.com',     // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
+            domain: 'p573ib0ut.bkt.clouddn.com',     // bucket 域名，下载资源时用到，如：'http://xxx.bkt.clouddn.com/' **必需**
             container: this.view.find('#uploadImageContainer'),             // 上传区域 DOM ID，默认是 browser_button 的父元素，
             max_file_size: '5mb',             // 最大文件体积限制
             max_retries: 3,                     // 上传失败最大重试次数
